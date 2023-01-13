1d3d26b2b2c55b3f51d78e29b26cd5676bffb28e
Nutan Kumari <nutan.pes@gmail.com>
2023-01-13 14:04:30 -0500

    initial commit

---
+++ app/main.py
@@ -0,0 +1,14 @@
+import module
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    val = module.print_f()
+    return val
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001) 
---
+++ app/module.py
@@ -0,0 +1,3 @@
+
+def print_f():
+   return "This is a flask application by Nutan"
